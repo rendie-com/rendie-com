3f5f95a2eeb961d7206164d0e9f9c812ba60d882
rendie <121609433+rendie-com@users.noreply.github.com>
2024-09-08 17:25:22 +0800

    Delete nextjs directory

--- nextjs/src/common/self/process.js
+++
@@ -1,9 +0,0 @@
-export const self_process = {
-  a01: async function (obj) {
-    let nObj = null
-    switch (obj.fun) {
-      case "cwd": nObj = process.cwd(); break;
-    }
-    return nObj;
-  },
-};
