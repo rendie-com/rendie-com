abeafb01a8f3c6600896f0200130cc59c9b0b97a
rendie <121609433+rendie-com@users.noreply.github.com>
2024-09-08 18:40:02 +0800

    Add files via upload

--- nextjs/src/pages/api/[[...arr]].js
+++ nextjs/src/pages/api/[[...arr]].js
@@ -1,8 +1,11 @@
+import { fs } from 'fs'
+import { path } from 'path'
+
 export default async function handler(req, res) {
-  const fs = require('fs'); 
-  const path = require('path'); 
-  //console.log(path.join(process.cwd(),'/src/app/img/ad.jpg'))
-  fs.readFile(path.join(process.cwd(),'/src/app/img/ad.jpg'), (err, data) => {
+
+  let dbpath = path.join(process.cwd(), "/src/img/ad.jpg")
+  //console.log(path.join(process.cwd(),'/src/img/ad.jpg'))
+  fs.readFile(dbpath, (err, data) => {
     res.setHeader('content-type', 'image/jpg')
     res.end(data)
   })
