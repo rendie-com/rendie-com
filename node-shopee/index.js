9b40e131deb43d1eb7df5f842ccc9ce5f7548ca8
rendie <121609433+rendie-com@users.noreply.github.com>
2024-09-11 22:53:54 +0800

    Update index.js

--- node-shopee/index.js
+++ node-shopee/index.js
@@ -37,14 +37,18 @@ import path from 'path';
   await page.goto('http://localhost:3000/admin.html');
   //////////////////////////////
   //通过page.evaluate进行localStorage 设置
-  await page.evaluate(() => {
+    await page.evaluate(oo => {
     localStorage.setItem("expires_in", 1722844755);
-    
-        localStorage.setItem("refresh_token", process.env.NODE_SHOPEE_REFRESH_TOKEN);
-        localStorage.setItem("access_token", process.env.NODE_SHOPEE_ACCESS_TOKEN);
-        localStorage.setItem("name", process.env.NODE_SHOPEE_NAME);
-    
+    //////////////////////////////////////
+    localStorage.setItem("refresh_token", oo.refresh_token);
+    localStorage.setItem("access_token", oo.access_token);
+    localStorage.setItem("name", oo.name);      
+    ///////////////////////////////////////////////////
     localStorage.setItem("menuList", '{"top1":1,"top2":{"18":{"name":"任务","id":"18","isbool":true,"url":"http://localhost:3000/view/Default/admin/html/Shopee/%E4%BB%BB%E5%8A%A1.html?jsFile=js02&return=%2Fview%2FDefault%2Fadmin%2Fhtml%2FShopee%2F%25E4%25BB%25BB%25E5%258A%25A1.html%3FjsFile%3Djs04"}}}');
+  },{
+    refresh_token:process.env.NODE_SHOPEE_REFRESH_TOKEN,
+    access_token:process.env.NODE_SHOPEE_ACCESS_TOKEN,
+    name:process.env.NODE_SHOPEE_NAME,
   });
   /////////////////////////////////// 
   let count = 0;
