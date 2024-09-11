ef8b8a7e3901871e1087de560a69b6ffe5699667
rendie <121609433+rendie-com@users.noreply.github.com>
2024-09-11 21:54:59 +0800

    Update index.js

--- node-shopee/index.js
+++ node-shopee/index.js
@@ -39,9 +39,9 @@ import path from 'path';
   //通过page.evaluate进行localStorage 设置
   await page.evaluate(() => {
     localStorage.setItem("expires_in", 1722844755);
-    localStorage.setItem("refresh_token", "0cee4a4acbd473a53b4faffb007751287070e2ea117f82b4b83913100954176b");
-    localStorage.setItem("access_token", "2ff68998b46cded1119bb2fa1406b7f84fdc988bd8bdef2374a379490eefc717");
-    localStorage.setItem("name", "gather");
+    localStorage.setItem("refresh_token", process.env.NODE_SHOPEE_REFRESH_TOKEN);
+    localStorage.setItem("access_token", process.env.NODE_SHOPEE_ACCESS_TOKEN);
+    localStorage.setItem("name", process.env.NODE_SHOPEE_NAME);
     localStorage.setItem("menuList", '{"top1":1,"top2":{"18":{"name":"任务","id":"18","isbool":true,"url":"http://localhost:3000/view/Default/admin/html/Shopee/%E4%BB%BB%E5%8A%A1.html?jsFile=js02&return=%2Fview%2FDefault%2Fadmin%2Fhtml%2FShopee%2F%25E4%25BB%25BB%25E5%258A%25A1.html%3FjsFile%3Djs04"}}}');
   });
   /////////////////////////////////// 
