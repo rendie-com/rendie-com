2be213ad7314cd3d34a1d9b01ce1df5babc2b15c
rendie <121609433+rendie-com@users.noreply.github.com>
2024-09-25 13:27:17 +0800

    Add files via upload

--- nextjs/public/view/Default/admin/js/Shopee/任务/定时任务/启动定时任务/所有任务/采集箱/粉丝/从店铺中获取粉丝.js
+++ nextjs/public/view/Default/admin/js/Shopee/任务/定时任务/启动定时任务/所有任务/采集箱/粉丝/从店铺中获取粉丝.js
@@ -27,6 +27,21 @@ var task = {
         this.a02(oo);
     },
     a02: function (oo) {
+        let data = [{
+            action: "fs",
+            fun: "access_sqlite",
+            database: "shopee/采集箱/粉丝/" + oo.site,
+            mode: 0,
+            elselist: [{
+                action: "fs",
+                fun: "download_sqlite",
+                url: "https://github.com/rendie-com/rendie-com/releases/download/1/shopee_gather_fans_" + oo.site + ".db",
+                database: "shopee/采集箱/粉丝/" + oo.site,
+            }]
+        }]
+        Tool.ajax.a01(data, this.a03, this, oo);
+    },
+    a03: function (t, oo) {
         $("#state").html("正在获取店铺信息。。。");
         let where = this.b01(oo.site)
         let data = [{
@@ -41,15 +56,15 @@ var task = {
                 sql: "select count(1) as total FROM @.table" + where,
             })
         }
-        Tool.ajax.a01(data, this.a03, this, oo);
+        Tool.ajax.a01(data, this.a04, this, oo);
     },
-    a03: function (t, oo) {
+    a04: function (t, oo) {
         //一次执行只获取前1个店铺的粉丝
         if (this.obj.C2 == 0) { this.obj.C2 = t[1][0].total > 3 ? 3 : t[1][0].total; }
         this.obj.Cobj = t[0][0];
-        Tool.x1x2("C", this.obj.C1, this.obj.C2, this.a04, this, this.e03, oo)
+        Tool.x1x2("C", this.obj.C1, this.obj.C2, this.a05, this, this.e03, oo)
     },
-    a04: function (oo) {
+    a05: function (oo) {
         this.obj.Cobj.new_get_follower_time = Tool.gettime("")
         $("#shopid").html(this.obj.Cobj.shopid)
         $("#get_follower_time").html(Tool.js_date_time2(this.obj.Cobj.get_follower_time))
@@ -80,7 +95,7 @@ var task = {
         let url = "https://" + (oo.site == "tw" ? "xiapi" : oo.site) + ".xiapibuy.com/api/v4/pages/get_follower_list?limit=20&offset=" + ((this.obj.D1 - 1) * 20) + "&shopid=" + this.obj.Cobj.shopid
         $("#url").html(url);
         $("#state").html("正在获取店铺的粉丝。。。")
-        gg.getFetch(url,"json", this.d03, this, oo)
+        gg.getFetch(url, "json", this.d03, this, oo)
     },
     d03: function (t, oo) {
         if (t.data) {
@@ -91,7 +106,7 @@ var task = {
                 }
                 else {
                     if (!t.data.nomore) { this.obj.D2++; }
-                    Tool.accounts.a01(t.data.accounts, false, oo.site, this.d04, this,oo);
+                    Tool.accounts.a01(t.data.accounts, false, oo.site, this.d04, this, oo);
                 }
             }
             else {
@@ -121,7 +136,7 @@ var task = {
         if (t[0].length == 0) {
             this.obj.C1++; this.obj.Cobj = {};
             this.obj.D1 = 1; this.obj.D2 = 1;
-            this.a02(oo);
+            this.a03("",oo);
         }
         else {
             Tool.pre(["出错11", t])
