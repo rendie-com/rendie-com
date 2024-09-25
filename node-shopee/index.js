ba9fd97ebfd08075d94e7bf671110aaceb2c67cb
rendie <121609433+rendie-com@users.noreply.github.com>
2024-09-25 19:16:02 +0800

    Update index.js

--- node-shopee/index.js
+++ node-shopee/index.js
@@ -69,6 +69,9 @@ import path from 'path';
     }
   }
   ///////////////////////
+  await page.screenshot({
+    path: '../releases/screenshot.png'
+  });
   await page.close()
   await browser.close()
   console.log('已完成所有任务。');
