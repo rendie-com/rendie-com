331dd3bfddbde94387b3ab45ee455fb566cd7a67
rendie <121609433+rendie-com@users.noreply.github.com>
2024-10-01 21:12:23 +0800

    Create ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+ mkdir ./releases/    
+ 
+ for i in {1..100};
+ do      
+   db_name="";
+   if (( $i < 10 ))
+   then        
+     db_name="00"+$i
+   else if (( $i < 100 ))
+   then 
+     db_name="0"+$i
+   fi    
+   echo $db_name
+ done
+
+
+         
+      
+        
+        # mv ./sqlite3/shopee/采集箱/商品/br.db ./releases/shopee_gather_product_br.db
+        # mv ./sqlite3/shopee/采集箱/商品/my.db ./releases/shopee_gather_product_my.db
+        # mv ./sqlite3/shopee/采集箱/商品/tw.db ./releases/shopee_gather_product_tw.db
+        
+        # 7z x nextjs.7z
+        # 7z x node-shopee.7z
+        # mv ./nextjs/plugins/ ./nextjs/public/plugins/
+        # unzip ./nextjs.zip
+        # unzip ./plugins.zip ./nextjs/
+        
+        # wget -O 1 https://github.com/rendie-com/rendie-com/releases/download/1/1.zip          
+        # mkdir ../images/
+        # cp image.tar ../images/image.tar
+        # tar -czvf ../images/image.tar.gz image.tar    
