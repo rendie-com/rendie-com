22bd8e22deb06e68e61cca4bc46551c154b89c55
rendie <121609433+rendie-com@users.noreply.github.com>
2024-10-04 16:27:23 +0800

    Update index.js

--- node-shopee/index.js
+++ node-shopee/index.js
@@ -69,6 +69,9 @@ import path from 'path';
       console.log("出错退出。")
     }
   }
+  await page.screenshot({
+      path: '../sqlite3/screenshot.png'
+  });
   await page.close()
   await browser.close()
   if (count < total) {
