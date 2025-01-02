85cdcb275c928f8bb0cb317ab6e1c5f8cbbb3fe9
rendie-com <rendie-com@users.noreply.github.com>
2025-01-02 15:14:12 +0000

    Deploying to main from @ rendie-com/rendie-com@bcad326d2d14656d30c30b93c7d2fd243ac57d27 🚀

---
+++ github-pages-deploy-action-temp-deployment-folder/sh/ssh.sh
@@ -0,0 +1,38 @@
+# !/bin/bash
+
+#   mkdir ./releases/    
+#  for i in {1..100}
+#  do  
+#    db_name="";
+#    if (( $i < 10 ))
+#    then        
+#      db_name="00$i"   
+#    elif (( $i < 100 ))
+#    then 
+#      db_name="0$i" 
+#    else
+#      db_name="$i"
+#    fi   
+#    mv ./br/${db_name}.db ./releases/shopee_gather_fans_br_${db_name}.db
+#    mv ./my/${db_name}.db ./releases/shopee_gather_fans_my_${db_name}.db
+#    echo "$db_name"
+#  done
+
+#  mv ./shopee_gather_fans_tw_038.db ./releases/shopee_gather_fans_tw_038.db
+        
+      
+        
+#         mv ./sqlite3/shopee/采集箱/商品/br.db ./releases/shopee_gather_product_br.db
+#         mv ./sqlite3/shopee/采集箱/商品/my.db ./releases/shopee_gather_product_my.db
+#         mv ./sqlite3/shopee/采集箱/商品/tw.db ./releases/shopee_gather_product_tw.db
+        
+#         7z x nextjs.7z
+#         7z x node-shopee.7z
+#         mv ./nextjs/plugins/ ./nextjs/public/plugins/
+#         unzip ./nextjs.zip
+#         unzip ./plugins.zip ./nextjs/
+        
+#         wget -O 1 https://github.com/rendie-com/rendie-com/releases/download/1/1.zip          
+#         mkdir ../images/
+#         cp image.tar ../images/image.tar
+#         tar -czvf ../images/image.tar.gz image.tar    
